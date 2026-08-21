4d4f3877fbdef4db5cc6eb4890783bf9b2457384
Wilson-Ochieng <wilsonochieng718@gmail.com>
2026-08-21 16:53:46 +0300

    created slider

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static List<String> bannerImage = [
+    "assets/images/banner/banner1.jpg",
+    "assets/images/banner/banner2.png",
+    "assets/images/banner/banner3.png",
+  ];
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:test_app/constants/app_colors.dart';
+import 'package:test_app/constants/app_constants.dart';
 import 'package:test_app/providers/ThemeProvider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,23 +15,43 @@ class HomeScreen extends StatelessWidget {
       // backgroundColor: AppColors.darkScaffoldColor,
       appBar: AppBar(title: Text("Shopify")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/banner/banner1.jpg'),
-            Text("Buy Now"),
-            ElevatedButton(onPressed: () {}, child: Text("Click me ")),
-            SwitchListTile(
-              title: Text(
-                themeProvider.getIsDarkTHeme ? "Dark theme" : "Light Theme",
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 200,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Swiper(
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        AppConstants.bannerImage[index],
+                        fit: BoxFit.cover,
+                      );
+                    },
+                    indicatorLayout: PageIndicatorLayout.COLOR,
+                    autoplay: true,
+                    itemCount: AppConstants.bannerImage.length,
+                    pagination: const SwiperPagination(),
+                    control: const SwiperControl(),
+                  ),
+                ),
               ),
-              value: themeProvider.getIsDarkTHeme,
-              onChanged: (wilson) {
-                themeProvider.setDarkTheme(wilson);
-                print("Theme state is ${themeProvider.getIsDarkTHeme}");
-              },
-            ),
-          ],
+              Text("Buy Now"),
+              ElevatedButton(onPressed: () {}, child: Text("Click me ")),
+              SwitchListTile(
+                title: Text(
+                  themeProvider.getIsDarkTHeme ? "Dark theme" : "Light Theme",
+                ),
+                value: themeProvider.getIsDarkTHeme,
+                onChanged: (wilson) {
+                  themeProvider.setDarkTheme(wilson);
+                  print("Theme state is ${themeProvider.getIsDarkTHeme}");
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
